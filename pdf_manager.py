ad02c6d26c4e95c64c27726619c820cca17933f3
RobiereMystere <robiere.mystere@gmail.com>
2022-01-21 16:50:15 +0100

    PDF MANAGER

--- pdf_manager.py
+++ pdf_manager.py
@@ -1,6 +1,7 @@
+import math
 import os
-
-from PyPDF2 import PdfFileReader
+import pdfplumber
+from pdfplumber.table import DEFAULT_MIN_WORDS_VERTICAL
 
 
 class PdfManager:
@@ -9,9 +10,26 @@ class PdfManager:
         super().__init__()
         files = os.listdir(".prices")
         for file in files:
-            with open(".prices/" + file, mode='rb') as f:
-                reader = PdfFileReader(f)
-                for page in reader.pages:
-                    print(page.extractText())
+            with pdfplumber.open(".prices/" + file) as pdf:
+                page = pdf.pages[0]
+                # for page in pdf.pages:
 
-            f.close()
+                # print(page.images)
+                # print(page.rects)
+                dict_char = page.chars
+                y0 = 0
+                x1 = 0
+                for t in dict_char:
+                    if y0 != t['y0']:
+                        print()
+                        y0 = t['y0']
+                    if math.fabs(t['x1'] - x1) > 10:
+                        #print(math.fabs(t['x1'] - x1))
+                        print(" ", end='')
+                    print(t['text'], end='')
+                    x1 = t['x1']
+                    """for lines in page.extract_tables({'horizontal_strategy': "text"}):
+                        for cells in lines:
+                            for cell in cells:
+                                if cell is not None:
+                                    print(cell)"""
